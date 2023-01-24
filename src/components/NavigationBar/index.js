1a5d92ceda5ce45b14ccc6047f65ea6e78771339
anasofiabeltran <anas.beltran206@gmail.com>
2023-01-24 14:45:30 -0500

    style

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import calendario from '../../assets/icons/calendario.png'
+import banderas from '../../assets/icons/banderas.png'
+import mecanico from '../../assets/icons/mecanico.png'
+import mensaje from '../../assets/icons/mensaje.png'
+import burbuja from '../../assets/icons/burbuja-de-chat.png'
+import reloj from '../../assets/icons/reloj-de-pared.png'
+import './NavigationBarCss.css'
 function NavigationBar() {
   return (
-    <div> AC </div>
+ 
+    <div className='NavigationBar'> 
+      <h4>AC </h4>
+      <section>
+        <button><img className='zoom' src={calendario}/></button>
+        <button><img className='zoom' src={reloj}/></button>
+        <button><img className='zoom' src={burbuja}/></button>
+        <button><img className='zoom' src={mensaje}/></button>
+        <button><img className='zoom' src={banderas}/></button>
+        <button><img className='zoom' src={mecanico}/></button>
+        
+      </section>
+        
+      
+      
+    </div>
+
+    
   )
 }
 
